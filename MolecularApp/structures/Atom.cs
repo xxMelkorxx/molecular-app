1718956564aeac53d58ec212f132ecf52bd66bec
SerBor <xxxserborxxx@gmail.com>
2023-09-27 22:03:44 +0300

    add structures

---
+++ MolecularApp/structures/Atom.cs
@@ -0,0 +1,31 @@
+﻿namespace MolecularApp;
+
+public enum AtomType
+{
+    Sn,
+    Si,
+    Ge
+}
+
+public class Atom
+{
+    public Vector Position;
+    public Vector Velocity;
+    public Vector Acceleration;
+    public AtomType Type;
+
+    /// <summary>
+    /// Индекс атома.
+    /// </summary>
+    public ushort Index { get; private set; }
+    
+    public Atom(ushort idx, AtomType atomType, Vector pos)
+    {
+        this.Type = atomType;
+        this.Position = pos;
+        this.Velocity = Vector.Zero;
+        this.Acceleration = Vector.Zero;
+        this.Index = idx;
+        this.Type = atomType;
+    }
+}
---
+++ MolecularApp/structures/Vector.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MolecularApp;
+
+public struct Vector
+{
+    public double X, Y, Z;
+
+    public Vector(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public static Vector Zero => new(0, 0, 0);
+
+    public static Vector One => new(1, 1, 1);
+
+    public double MaxElement() => Math.Max(Math.Max(X, Y), Math.Max(X, Z));
+
+    public double MinElement() => Math.Min(Math.Min(X, Y), Math.Min(X, Z));
+
+    public double SquaredMagnitude() => X * X + Y * Y + Z * Z;
+
+    public double Magnitude() => Math.Sqrt(SquaredMagnitude());
+
+    public static Vector operator +(Vector vec1, Vector vec2) => new(vec1.X + vec2.X, vec1.Y + vec2.Y, vec1.Z + vec2.Z);
+
+    public static Vector operator +(Vector vec1, int value) => new(vec1.X + value, vec1.Y + value, vec1.Z + value);
+
+    public static Vector operator +(Vector vec1, double value) => new(vec1.X + value, vec1.Y + value, vec1.Z + value);
+
+    public static Vector operator -(Vector vec1, Vector vec2) => new(vec1.X - vec2.X, vec1.Y - vec2.Y, vec1.Z - vec2.Z);
+
+    public static Vector operator -(Vector vec1, int value) => new(vec1.X - value, vec1.Y - value, vec1.Z - value);
+
+    public static Vector operator -(Vector vec1, double value) => new(vec1.X - value, vec1.Y - value, vec1.Z - value);
+
+    public static Vector operator *(Vector vec, int num) => new(vec.X * num, vec.Y * num, vec.Z * num);
+
+    public static Vector operator *(int num, Vector vec) => new(vec.X * num, vec.Y * num, vec.Z * num);
+
+    public static Vector operator *(Vector vec, double num) => new(vec.X * num, vec.Y * num, vec.Z * num);
+
+    public static Vector operator *(double num, Vector vec) => new(vec.X * num, vec.Y * num, vec.Z * num);
+
+    public static Vector operator /(Vector vec, int num) => num != 0 ? new Vector(vec.X / num, vec.Y / num, vec.Z / num) : throw new DivideByZeroException();
+
+    public static Vector operator /(Vector vec, double num) => num != 0 ? new Vector(vec.X / num, vec.Y / num, vec.Z / num) : throw new DivideByZeroException();
+}
